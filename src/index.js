a9774f6074e3669d95a775a90c36a4ae9309065f
Kevin Lin <kevinptt0323@gmail.com>
2017-06-16 02:19:04 +0800

    add iconv-lite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-import socket from 'socket.io-client';
+import { encode, decode } from 'iconv-lite';
 
+class pttio {
+  constructor(url = 'wss://ws.ptt.cc/bbs') {
+    this.initWS(url);
+  }
+  initWS(url) {
+    const socket = new WebSocket(url);
+    socket.onopen = () => {
+      this._onopen();
+    };
 
+    socket.onmessage = (msg) => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this._onmessage(decode(new Uint8Array(e.target.result), 'big5'));
+      }
+      reader.readAsArrayBuffer(msg.data);
+    };
+
+    this.socket = socket;
+  }
+
+  _onopen() {
+    if (typeof this.onmessage === 'function') {
+      this.onopen();
+    }
+  }
+
+  _onmessage(msg) {
+    if (typeof this.onmessage === 'function') {
+      this.onmessage(msg);
+    }
+  }
+}
+
+export default pttio;
