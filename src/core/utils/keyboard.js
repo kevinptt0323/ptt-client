283256445317268227d57266bc196e0dabe58680
Kevin Lin <kevinptt0323@gmail.com>
2017-06-16 04:20:10 +0800

    add keyboard

--- src/core/utils/keyboard.js
+++ src/core/utils/keyboard.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  Enter: '\r',
+  Left: '\x1b[D',
+  Right: '\x1b[C',
+  Up: '\x1b[A',
+  Down: '\x1b[B',
+  PgUp: '\x1b[5~',
+  PgDown: '\x1b[6~',
+  Home: '\x1b[1~',
+  End: '\x1b[4~',
+  CtrlZ: '\x1a',
+  CtrlP: '\x10',
+};
