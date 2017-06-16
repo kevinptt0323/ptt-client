4e5906b81321ae6564aff8577f119e7a3519fe0f
Kevin Lin <kevinptt0323@gmail.com>
2017-06-17 03:44:46 +0800

    use terminal.js to show content

--- src/sites/ptt/bot.js
+++ src/sites/ptt/bot.js
@@ -1,16 +1,21 @@
 import EventEmitter from 'eventemitter3';
 import { keyboard as key } from '../../utils';
+import Terminal2 from 'terminal.js';
 
 class bot extends EventEmitter {
   constructor(Socket, config) {
     super();
-    this._config = config;
+    this._parser = config.parser;
+    this._term2 = new Terminal2({columns: 80, rows: 24});
+
     const socket = new Socket(config);
     socket.onconnect = this.emit.bind(this, 'connect');
     socket.onmessage = this.emit.bind(this, 'message');
     socket.connect();
+
     this.on('message', (msg) => {
-      this.lastmessage = msg;
+      this._term2.write(msg);
+      this.emit('redraw', this._term2.toString());
     });
     this._socket = socket;
   }
@@ -19,7 +24,7 @@ class bot extends EventEmitter {
     return new Promise(resolve => {
       this._socket.send(`${username}${key.Enter}${password}${key.Enter}`);
       setTimeout(() => {
-        resolve(this.lastmessage);
+        resolve(this._term2.toString());
       }, 2000);
     });
   }
