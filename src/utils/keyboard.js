dfd817ba5e48899ef2e5addfa0da93ef779149d0
Kevin Lin <kevinptt0323@gmail.com>
2017-06-17 03:47:47 +0800

    add backspace key

--- src/utils/keyboard.js
+++ src/utils/keyboard.js
@@ -1,13 +1,14 @@
 export default {
   Enter: '\r',
-  Left: '\x1b[D',
-  Right: '\x1b[C',
-  Up: '\x1b[A',
-  Down: '\x1b[B',
+  ArrowLeft: '\x1b[D',
+  ArrowRight: '\x1b[C',
+  ArrowUp: '\x1b[A',
+  ArrowDown: '\x1b[B',
   PgUp: '\x1b[5~',
   PgDown: '\x1b[6~',
   Home: '\x1b[1~',
   End: '\x1b[4~',
+  Backspace: '\b',
   CtrlZ: '\x1a',
   CtrlP: '\x10',
 };
